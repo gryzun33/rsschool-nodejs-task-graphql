33d78ab89265e3c07b2387df98717413d0e32753
gryzun33 <yakushevao777@gmail.com>
2024-10-30 16:39:49 +0300

    feat: add fields to type user

--- src/routes/graphql/types/users.ts
+++ src/routes/graphql/types/users.ts
@@ -1,11 +1,29 @@
-import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLNonNull } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLNonNull,
+  GraphQLList,
+} from 'graphql';
 import { UUIDType } from './uuid.js';
+import { Profile } from './profiles.js';
+import { Post } from './posts.js';
 
-export const User = new GraphQLObjectType({
+const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: new GraphQLNonNull(UUIDType) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     balance: { type: new GraphQLNonNull(GraphQLFloat) },
-  },
+    profile: { type: Profile },
+    posts: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Post))) },
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+  }),
 });
+
+export const User = UserType;
